fc20bbb3eaa2a0a9b2205236857c0d8a7f742516
Muhammad Ariful Furqon, S.Pd., M.Kom <ariful.furqon@unej.ac.id>
2022-09-05 17:20:50 +0700

    Remove

--- Minggu 04 - Fungsi Rekursif/05 latihan 3 perkalian/2pangkatndaa.py
+++
@@ -1,7 +0,0 @@
-def pangkatdua(n):
-    if n == 0:
-        return 1
-    else:
-        return pangkatdua(n-1) + pangkatdua(n-1)
-    
-print (pangkatdua(4))
--- Minggu 05 - Array/01 Preview Rekursif/non-recursive.py
+++
@@ -1,7 +0,0 @@
-def factorial(number):
-    product = 1
-    for i in range (number):
-        product = product * (i+1)
-    return product
-
-print(factorial(3))
